16234a9a9a07dda5c1c52fd8ef537c1470d2831b
Awal hossain <waqin21@gmail.com>
2022-08-07 16:58:26 +0600

    first commit

---
+++ Day-01/index.js
@@ -0,0 +1,37 @@
+/**
+ * NodeJs day-01 content
+ */
+
+/** Dependencies */
+
+const http = require('http');
+const host = "127.0.2.1";
+const port = 5000
+const fs = require('fs')
+
+var server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello World!");
+
+    /** Promise m */
+    fs.promises.readFile('index.html')
+    .then(resl=> res.end(resl))
+    .catch((err)=> console.log(err))
+
+    /** Callback method */
+    // fs.readFile('index.html',(err,data)=>{
+    //     if(err){
+    //         throw err;
+    //     }
+    //     res.end(data);
+    // })
+  });
+
+
+
+  server.listen(port,host, (error) => {  
+    if (error) {
+        return console.log('Error occured : ', error );
+      }
+    console.log('server is listening on ' + host + ':'+ port)
+    ;})
