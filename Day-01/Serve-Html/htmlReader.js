cc0eaa82f69acd236301afdb05c0c3ea1873b708
Awal hossain <waqin21@gmail.com>
2022-08-13 11:24:27 +0600

    modified html reader 👍

--- Day-01/Serve-Html/htmlReader.js
+++ Day-01/Serve-Html/htmlReader.js
@@ -5,33 +5,19 @@
 /** Dependencies */
 
 const http = require('http');
-const host = "127.0.2.1";
-const port = 5000
-const fs = require('fs')
-
-var server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello World!");
-
-    /** Promise method */
-    fs.promises.readFile('index.html')
-    .then(resl=> res.end(resl))
-    .catch((err)=> console.log(err))
-
-    /** Callback method */
-    // fs.readFile('index.html',(err,data)=>{
-    //     if(err){
-    //         throw err;
-    //     }
-    //     res.end(data);
-    // })
-  });
+// const 
 
 
 
-  server.listen(port,host, (error) => {  
-    if (error) {
-        return console.log('Error occured : ', error );
-      }
-    console.log('server is listening on ' + host + ':'+ port)
-    ;})
+const server = http.createServer((req, res)=>{
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+
+  res.write("<h1>hello there</h1>");
+  res.end("end it")
+})
+
+
+server.listen(3000,()=>{
+  console.log(`Hey this server is runnin on 3000`);
+})
