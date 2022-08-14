9ad56b1eb18bf47f8f79cc0e35c78e7674fcee4b
Awal hossain <waqin21@gmail.com>
2022-08-14 16:23:09 +0600

    data & file appending works fine 🚀🚀�

--- FileSystem/Append-File/index.js
+++ FileSystem/Append-File/index.js
@@ -11,9 +11,9 @@ const fs = require('fs')
 var server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
 
-    /**  */
-    fs.promises.writeFile('file.txt', "this is the data file i am writing let's see w")
-    .then(resl=> res.end("File has been created with data"))
+    /** appending file useing append */
+    fs.promises.appendFile('file.txt', "this is the data file i am writing let's see w")
+    .then(resl=> res.end("File has been appended with data"))
     .catch((err)=> console.log(err))
 
   });
