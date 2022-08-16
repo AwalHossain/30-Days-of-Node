94adc4370b913e7873d3050728a43f96856c3f1e
Awal hossain <waqin21@gmail.com>
2022-08-16 20:24:57 +0600

    console strderr & stdout is processing fine 🥳

--- console Module/new_console.js
+++ console Module/new_console.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
+const {Console} = require('console')
+const output = fs.createWriteStream('./stdout.log');
+const errOutput = fs.createWriteStream("./stderr.log")
 
+const print = new Console(output, errOutput);
+
+const roll = 838934;
+print.log('roll: %d %d', roll);
+print.log("this will be stored in a file")
 
 const server = http.createServer((res, req)=>{
     
-    res.writeHead(200, {"Content-Type":"text/"})
+    res.writeHead(200, {"Content-Type":"text/plain"})
+
 });
 
 
