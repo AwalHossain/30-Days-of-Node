9026db59b88af893717a19dcbdfe5b18ae3d8aa0
Awal hossain <waqin21@gmail.com>
2022-08-18 10:26:44 +0600

    created read stream to read data as stream 👌

--- DataHandling/DataStreaming/stream.js
+++ DataHandling/DataStreaming/stream.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 /** Read the file chunk by chunk */
 let readStream = fs.createReadStream("readFile.txt");
 
+
 /** listen readstring.on data event */
+
+
 readStream.on('data', (data)=>{
     console.log(`the data is ${data}`);
 })
@@ -15,6 +18,7 @@ readStream.on('data', (data)=>{
 readStream.once('data', data=>{
     console.log("Reading data once", data);
 })
+
 
 
 /** listen end event to ensure reading streaming is end */
