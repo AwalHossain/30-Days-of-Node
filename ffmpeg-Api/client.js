44397c36db16a5501a69fe5eba679eb6eacf2a54
Awal hossain <waqin21@gmail.com>
2022-10-03 19:47:36 +0600

    create thumbnail 📌

--- ffmpeg-Api/client.js
+++ ffmpeg-Api/client.js
@@ -3,10 +3,33 @@ const submitButton = document.querySelector('#submit');
 const thumbnailPreview = document.querySelector('#thumbnail');
 const errorDiv = document.querySelector('#error');
 
+
+const API_ENDPOINT = 'http://localhost:3000/thumbnail'
+
+// show error message if any
 function showError(msg){
     errorDiv.innerText = `Error: ${msg}`
 }
 
-submitButton.addEventListener('click', ()=>{
+
+
+// submit file 
+submitButton.addEventListener('click', async()=>{
     const {files} = fileInput;
 })
+
+
+async function createThumbnail(video){
+
+    const payload = new FormData();
+    payload.append('video', video);
+
+    const res = await fetch(API_ENDPOINT, {
+        method: 'Post',
+        body: payload
+    })
+
+    if(!res.ok){
+        throw new Error(`Creating thumbnil failed`);
+    }
+}
